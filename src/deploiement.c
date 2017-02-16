cd21563fcd21647bbc9f032d0c8b032393231fb0
Nono-G <noe.goudian@etu.univ-amu.fr>
2017-02-16 10:38:14 +0100

    Nono : correction eval

--- src/deploiement.c
+++ src/deploiement.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]){
 		Data* data = load_instance(argv[1]);
 		printf("%d \n", data->connection[data->facility_count][data->client_count]);
 		printf("%d \n", data->connection[1][1]);
-		int test[2] = {1,1};
+		int test[2] = {0,0};
 		printf("eval(O) : %d\n",eval(data,test));
 		free_data(data);
 	}
@@ -20,29 +20,38 @@ int main(int argc, char *argv[]){
 }
 
 int eval (Data* data, int* ouverts){
+
 	int sum = 0;
 	//Somme Couts ouverture
 	int i = 1;
 	while(i <= data->facility_count){
-		if(ouverts[i]){
+		if(ouverts[i-1]){
 			//printf("ouverture : %d\n", data->opening_cost[i]);
 			sum += data->opening_cost[i];
 		}
 		i++;
 	}
+	//Somme couts connexion
 	int j = 1;
 	while(j <= data->client_count){
-		i=2;
-		int iMin = 1;
+		i=1;
+		while(!ouverts[i-1]){
+			i++;
+		}
+		int iMin = i;
 		while(i <= data->facility_count){
-			if(data->connection[i][j] < data->connection[iMin][j]){
+			if(ouverts[i-1] && (data->connection[i][j] < data->connection[iMin][j])){
 				iMin = i;
 			}
 			i++;
 		}
 		//printf("iMin : %d\nConnection : %d\n", iMin,data->connection[iMin][j]);
-		sum += data->connection[iMin][j];
+		if(iMin <= data->facility_count){
+			sum += data->connection[iMin][j];
+		}
 		j++;
 	}
 	return sum;
 }
+
+
