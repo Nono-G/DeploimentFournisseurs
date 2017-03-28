98fc415b464bf080879c075d4513b101a3d6af6b
Nono-G <noe.goudian@etu.univ-amu.fr>
2017-03-28 16:43:08 +0200

    N: nouveau tri, ça marche bien glouton 2

--- inc/tris.h
+++ inc/tris.h
@@ -1,8 +1,9 @@
-#ifndef __QUICKSORT_H__
-#define __QUICKSORT_H__
+#ifndef __TRIS_H__
+#define __TRIS_H__
 
 #include "util.h"
 
 void tri_rapide(Data* data, int fournisseur_i, int* array, int arraysize);
+void tri_lent(Data* data, int fournisseur_i, int* tab, int len);
 
 #endif
